5a3e4e4930bbd41f5d9b7f2eb2440b625980f4f0
Alex Anderson <aanderson65@wisc.edu>
2020-01-13 16:37:13 -0600

    Upgrade to PC

--- profile.py
+++ profile.py
@@ -13,7 +13,7 @@ import geni.rspec.pg as pg
 
 request = portal.context.makeRequestRSpec()
 
-node = request.XenVM("node")
+node = request.RawPC("node")
 
 # Ubuntu 16.04 LTS 64-bit
 node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU16-64-STD"
