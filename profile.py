3afb271598d34b458131621b2dba4dcef924ddf8
Alex Anderson <aanderson65@wisc.edu>
2020-03-10 17:05:26 -0500

    Parameterize install

--- profile.py
+++ profile.py
@@ -1,5 +1,5 @@
 """
-Allocates a PC running Ubuntu 18 and installs Docker CE
+Allocates a PC running Ubuntu 18
 
 Instructions:
 Wait for the profile instance to start, and then log in to the VM via the
@@ -11,16 +11,29 @@ a public IP address for the VM itself.)
 import geni.portal as portal
 import geni.rspec.pg as pg
 
+portal.context.defineParameter( "image", "Image", portal.ParameterType.IMAGE, "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU18-64-STD" )
+portal.context.defineParameter( "docker", "Install Docker?", portal.ParameterType.BOOLEAN, True )
+portal.context.defineParameter( "p3_tools", "Install Python3 tools?", portal.ParameterType.BOOLEAN, False )
+portal.context.defineParameter( "pyspark", "Install PySpark?", portal.ParameterType.BOOLEAN, False )
+
+# Retrieve the values the user specifies during instantiation.
+params = portal.context.bindParameters()
+
 request = portal.context.makeRequestRSpec()
 
-num_pcs = 1
-for i in range(0, num_pcs):
-    node = request.RawPC("node-" + str(i))
+node = request.RawPC("node")
 
-    # Ubuntu 18.04 LTS 64-bit
-    node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU18-64-STD"
+node.disk_image = params.image
 
-    # Intall Docker
+if params.docker:
     node.addService(pg.Execute(shell="bash", command="/local/repository/docker.bash"))
+
+if params.p3_tools:
+    node.addService(pg.Execute(shell="bash", command="/local/repository/python.bash"))
+
+if params.pyspark:
+    if not params.p3_tools:
+        node.addService(pg.Execute(shell="bash", command="/local/repository/python.bash"))
+    node.addService(pg.Execute(shell="bash", command="/local/repository/pyspark.bash"))
 
 portal.context.printRequestRSpec()
