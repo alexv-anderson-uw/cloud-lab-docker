dd9e13413195ae765a30ca4a26fecdcd58b12ec6
Alex Anderson <aanderson65@wisc.edu>
2020-05-20 15:39:56 -0500

    Fix documentation

--- profile.py
+++ profile.py
@@ -2,17 +2,15 @@
 Allocates a PC running Ubuntu 18
 
 Instructions:
-Wait for the profile instance to start, and then log in to the VM via the
-ssh port specified below.  (Note that in this case, you will need to access
-the VM through a high port on the physical host, since we have not requested
-a public IP address for the VM itself.)
+Wait for the profile instance to start, and then log in to the VM via SSH.
+Temporary HD partition will be mounted to /bs_part
 """
 
 import geni.portal as portal
 import geni.rspec.pg as pg
 
 portal.context.defineParameter( "image", "Image", portal.ParameterType.IMAGE, "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU18-64-STD" )
-portal.context.defineParameter( "extra_hd_size", "Install PySpark?", portal.ParameterType.INTEGER, 0 )
+portal.context.defineParameter( "extra_hd_size", "Size of temporary HD partition", portal.ParameterType.INTEGER, 0 )
 portal.context.defineParameter( "docker", "Install Docker?", portal.ParameterType.BOOLEAN, True )
 portal.context.defineParameter( "p3_tools", "Install Python3 tools?", portal.ParameterType.BOOLEAN, False )
 portal.context.defineParameter( "pyspark", "Install PySpark?", portal.ParameterType.BOOLEAN, False )
